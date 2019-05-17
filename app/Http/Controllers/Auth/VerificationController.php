db9e3519d6f62f473c7d43a6fc6b2063c058ab86
xiwu962464 <85954875@qq.com>
2019-05-17 15:18:14 +0800

    认证后的消息提醒

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -34,7 +34,9 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
+        // 设定了所有的控制器动作都需要登录后才能访问
         $this->middleware('auth');
+        // 设定了 只有 verify 动作使用 signed 中间件进行认证
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
