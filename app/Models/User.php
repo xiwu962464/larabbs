de3a658e886136e2429e6bbcfece3a38f574c395
xiwu962464 <85954875@qq.com>
2019-05-20 08:46:47 +0000

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -75,4 +75,12 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Reply::class);
     }
+
+    // 去除顶部未读消息标示
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
 }
