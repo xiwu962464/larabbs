1966f87a8f3da504102716348c6d4fb557896d96
xiwu962464 <85954875@qq.com>
2019-05-20 09:12:49 +0000

    删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -58,4 +58,11 @@ class Topic extends Model
     {
         return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
+
+    // 当回复被删除后，评论数已变更，更新话题的 reply_count
+    public function updateReplyCount()
+    {
+        $this->reply_count = $this->replies->count();
+        $this->save();
+    }
 }
