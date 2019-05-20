1013e8f98b46804d4c2ee73a112595555f0b0686
xiwu962464 <85954875@qq.com>
2019-05-20 09:46:19 +0000

    站点权限部署

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,17 +7,19 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * 注册任何申请服务
      *
      * @return void
      */
     public function register()
     {
-        //
+        if (app()->isLocal()) {
+            $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+        }
     }
 
     /**
-     * Bootstrap any application services.
+     * 引导任何应用程序服务。
      *
      * @return void
      */
@@ -27,6 +29,10 @@ class AppServiceProvider extends ServiceProvider
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
 
-        //
+        // 默认只能在 local 环境中访问仪表盘
+        \Horizon::auth(function ($request) {
+            // 是否是站长
+            return \Auth::user()->hasRole('Founder');
+        });
     }
 }
